226d5095b27385305d35633bb3821c57e1b6a968
Gabriela Miranda <gabi.m.domingues@hotmail.com>
2021-11-23 23:38:57 -0300

    colisoes e score

--- Assets/GLDraw.cs
+++ Assets/GLDraw.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GLDraw : MonoBehaviour
@@ -8,9 +9,21 @@ public class GLDraw : MonoBehaviour
   public Vector2 sb;
   public List<Food> foods = new List<Food>();
 
+  public float cx=0;
+  public float cy;
+  public float velo = 2.5f;
+  public float radius = 2;
+  public Material fig;
+  public Vector3 center = new Vector3(0, 0, 0);
+
+  public int scoreGame = 0;
+  public Text scoreText;
+
   private void Start()
   {
     sb = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    cx = 0;
+    cy = -sb.y + radius+ 3.1f;
     InvokeRepeating("RainFood", 0.0f, 0.1f);
     InvokeRepeating("CreateMeatball", 0.0f, 1.3f);
     InvokeRepeating("CreatePizza", 7.0f, 7.0f);
@@ -20,11 +33,21 @@ public class GLDraw : MonoBehaviour
   private void Update()
   {
     sb = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    if(Input.GetKey(KeyCode.LeftArrow)){
+      if(cx>-sb.x)
+        cx -= velo;
+    }
+    if(Input.GetKey(KeyCode.RightArrow)){
+      if(cx+3.5f<sb.x)
+        cx += velo;
+    }
+    scoreText.text= "Score: " + scoreGame;
   }
 
   private void OnPostRender()
   {
     Ground();
+    Cesta();
 
     foods.ForEach(delegate (Food food)
     {
@@ -39,6 +62,11 @@ public class GLDraw : MonoBehaviour
       food.y -= food.dropSpeed;
       food.yBoundaries[0] -= food.dropSpeed;
       food.yBoundaries[1] -= food.dropSpeed;
+      if(food.yBoundaries[0] <= cy && food.xBoundaries[0]>= cx && food.xBoundaries[1]<= cx+3.5f){
+        scoreGame += food.score;
+        foods.Remove(food);
+      }
+      
     });
 
     foods.RemoveAll(food => food.yBoundaries[0] < sb.y * (-1) + 3);
@@ -108,7 +136,7 @@ public class GLDraw : MonoBehaviour
     public Meatball(float xPos, float yPos, Material material) : base(xPos, yPos, material)
     {
       score = 100;
-      dropSpeed = 0.2f;
+      dropSpeed = 0.4f;
     }
 
     public override void DrawFood()
@@ -257,5 +285,47 @@ public class GLDraw : MonoBehaviour
       GL.End();
       GL.PopMatrix();
     }
+  }
+  void Cesta(){
+    Color color = new Color(0.4f, 0.2f, 0.15f, 1f);
+
+    GL.PushMatrix();
+    mat.SetPass(0);
+    GL.Begin(GL.QUADS);
+    GL.Color(color);
+
+    GL.Vertex3(cx, cy, 0);
+    GL.Vertex3(cx, cy-1, 0);
+    GL.Vertex3(cx + 3.5f, cy-1, 0);
+    GL.Vertex3(cx + 3.5f, cy, 0);
+
+    GL.End();
+    GL.PopMatrix();
+
+    GL.PushMatrix();
+    mat.SetPass(0);
+    GL.Begin(GL.QUADS);
+    GL.Color(color);
+
+    GL.Vertex3(cx+0.4f, cy, 0);
+    GL.Vertex3(cx+0.4f, cy - 1.5f, 0);
+    GL.Vertex3(cx + 3.1f, cy - 1.5f, 0);
+    GL.Vertex3(cx + 3.1f, cy, 0);
+
+    GL.End();
+    GL.PopMatrix();
+
+    GL.PushMatrix();
+    mat.SetPass(0);
+    GL.Begin(GL.QUADS);
+    GL.Color(color);
+
+    GL.Vertex3(cx+0.8f, cy, 0);
+    GL.Vertex3(cx+0.8f, cy - 1.8f, 0);
+    GL.Vertex3(cx + 2.7f, cy - 1.8f, 0);
+    GL.Vertex3(cx + 2.7f, cy, 0);
+
+    GL.End();
+    GL.PopMatrix();
   }
 }
